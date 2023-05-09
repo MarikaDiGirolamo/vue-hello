69d909753cba01c7d45a837ecb786d696f20bf08
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-05-09 10:17:57 +0200

    Add kenobi

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ createApp ({
         return{
             titolo: "Hello there!",
             chiave: "General Kenobi",
-            percorsoImg: "https://w7.pngwing.com/pngs/275/235/png-transparent-star-wars-rebel-alliance-logo-star-wars-rebellion-rebel-alliance-senator-bail-organa-galactic-civil-war-star-wars-logo-smiley-sign-thumbnail.png"
+            percorsoImg: "https://www.swx.it/databank/images/thumb/4/47/Obi-wan_kenobi_27.jpg/300px-Obi-wan_kenobi_27.jpg"
 
         }
     },
